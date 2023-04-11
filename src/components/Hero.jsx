e6e3c6953e84ef543d55edf73e66e27354a22a60
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-04-11 20:43:37 +0200

    added Heading text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,6 +12,21 @@ const Hero = () => {
           <span>the best fitness club in the town</span>
         </div>
         {/*Hero Heading */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">Shape </span>
+            <span>your</span>
+          </div>
+          <div>
+            <span>ideal body</span>
+          </div>
+          <div>
+            <span>
+              We will help you shape and build your ideal body and live up your
+              life to the fullest.
+            </span>
+          </div>
+        </div>
       </div>
       <div className="right-h">right</div>
     </div>
