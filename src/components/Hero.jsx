d91703fa41e0f9262e36dc4dfd109092323f152f
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-04-13 10:03:36 +0200

    added and styled hero images/ calories and heart rate sections

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import "./Hero.css";
 import Header from "./Header";
+import hero_image from "../assets/hero_image.png";
+import hero_image_back from "../assets/hero_image_back.png";
+import Heart from "../assets/heart.png";
+import Calories from "../assets/calories.png";
+
 const Hero = () => {
   return (
     <div className="hero">
@@ -50,7 +55,28 @@ const Hero = () => {
           <buttons className="btn">Learn More</buttons>
         </div>
       </div>
-      <div className="right-h">right</div>
+      <div className="right-h">
+        <button className="btn">Join Now</button>
+
+        <div className="heart-rate">
+          <img src={Heart} alt="" />
+          <span>Heart Rate</span>
+          <span>116 bpm</span>
+        </div>
+
+        {/*hero images*/}
+        <img src={hero_image} alt="" className="hero-image" />
+        <img src={hero_image_back} alt="" className="hero-image-back" />
+
+        {/*calories*/}
+        <div className="calories">
+          <img src={Calories} alt="" />
+          <div>
+            <span>Calories Burned</span>
+            <span>220 kcal</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
