3d96a1939d8fffa537530da73ec4761ff99eb1d1
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-04-16 16:22:26 +0200

    added Reasons component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Hero from "./components/Hero";
 import Programs from "./components/Programs";
+import Reasons from "./components/Reasons";
+
 function App() {
   return (
     <div className="App">
       <Hero />
       <Programs />
+      <Reasons />
     </div>
   );
 }
