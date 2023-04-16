af31bdbb22072fb000df9333fd7915a2f1b6fc73
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-04-16 17:18:35 +0200

    added and styled the resons section

--- src/components/Reasons.jsx
+++ src/components/Reasons.jsx
@@ -12,7 +12,45 @@ import tick from "../assets/tick.png";
 const Reasons = () => {
   return (
     <div className="Reasons" id="reasons">
-      Reasons
+      <div className="left-r">
+        <img src={image1} alt="" />
+        <img src={image2} alt="" />
+        <img src={image3} alt="" />
+        <img src={image4} alt="" />
+      </div>
+      <div className="right-r">
+        <span>some reasons</span>
+        <div>
+          <span className="stroke-text">why</span>
+          <span>choose us?</span>
+        </div>
+        <div className="details-r">
+          <div>
+            <img src={tick} alt="" />
+            <span>OVER 140+ EXPERT COACHS</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>TRAIN SMARTER AND FASTER THAN BEFORE</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>1 FREE PROGRAM FOR NEW MEMBER</span>
+          </div>
+          <div>
+            <img src={tick} alt="" />
+            <span>RELIABLE PARTNERS</span>
+          </div>
+        </div>
+        <span style={{ color: "var(--gray)", fontWeight: "normal" }}>
+          OUR PARTNERS
+        </span>
+        <div className="partners">
+          <img src={nb} alt="" />
+          <img src={adidas} alt="" />
+          <img src={nike} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
