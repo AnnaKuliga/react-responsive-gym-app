48ac67b3f80502cca16ea37d947dc1268b65a00b
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-04-22 20:24:59 +0200

    Added testimonials section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import Plans from "./components/Plans";
 import Programs from "./components/Programs";
 import Reasons from "./components/Reasons";
+import Testimonials from "./components/Testimonials";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Programs />
       <Reasons />
       <Plans />
+      <Testimonials />
     </div>
   );
 }
