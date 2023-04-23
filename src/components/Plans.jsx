192754b15157043475124ee4b0570487cb217de3
Anna <118673659+AnnaKuliga@users.noreply.github.com>
2023-04-23 11:48:12 +0200

    Added blurs to the background in all sections

--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -6,6 +6,8 @@ const Plans = () => {
   return (
     <div className="Plans">
       <div className="plans-container">
+        <div className="blur plans-blur-1"></div>
+        <div className="blur plans-blur-2"></div>
         <div className="programs-header" style={{ gap: "2rem" }}>
           <span className="stroke-text">READY TO START</span>
           <span>YOUR JOURNEY</span>
